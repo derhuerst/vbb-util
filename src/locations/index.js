f904d370c4d8e5c1354002fe7e9039948f950947
Jannis R <mail@jannisr.de>
2015-08-08 05:54:28 +0200

    `src/locations/index`: added `parse`

--- src/locations/index.js
+++ src/locations/index.js
@@ -1,3 +1,7 @@
+var products = require('../products');
+
+
+
 module.exports = {
 
 	stations:	require('./stations/index'),
@@ -5,6 +9,27 @@ module.exports = {
 
 
 
+	parse: function (location) {
+		var result = {
+			name:		location.name,
+			latitude:	location.lat,
+			longitude:	location.lon
+		};
 
+		if (this.types[location.type])
+			result.type = this.types[location.type.toUpperCase()];
+		if (!result.type || result.type.type === 'station') {
+			if (location.extId) {   // `loc` is a station
+				result.id =			this.stations.parseId(location.extId);
+				result.type = 		this.types.station;
+				result.products =	products.parseBitmask(location.products);
+				if (location.LocationNotes)
+					result.notes = this.stations.notes.parse(location.LocationNotes);
+			} else result.type = this.types.unknown;
+		}
+
+		result.type = result.type.type;
+		return result;
+	}
 
 };
