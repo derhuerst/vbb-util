5fa0adaa75fcbb483901d087e3e46c761a787eee
Jannis R <mail@jannisr.de>
2015-08-08 05:54:53 +0200

    finished `src/routes/legs/notes`

--- src/routes/legs/notes.js
+++ src/routes/legs/notes.js
@@ -1,1 +1,31 @@
-module.exports = 'todo';
+// todo: see derhuerst/vbb#9
+
+var n = module.exports = {
+
+	handicapAccessible: {
+		id:		'BF',
+		type:	'handicapAccessible'
+	},
+
+	bycicleConveyance: {
+		id:		'FB',
+		type:	'bycicleConveyance'
+	},
+
+};
+
+
+
+n.BF = n.handicapAccessible;
+n.FB = n.bycicleConveyance;
+
+
+
+n.parse = function (notes) {
+	var result = {}, i, property;
+	for (i in notes.Note) {
+		property = n[notes.Note[i].key.toUpperCase()];
+		if (property) result[property.type] = true;
+	}
+	return result;
+};
