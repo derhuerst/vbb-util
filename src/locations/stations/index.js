3ae56c0992019d8662d182b467034e9f68c16648
Jannis R <mail@jannisr.de>
2015-08-13 19:15:36 +0200

    `src/locations/stations/index`: added `searchableName`

--- src/locations/stations/index.js
+++ src/locations/stations/index.js
@@ -4,6 +4,38 @@ module.exports = {
 
 
 
+	searchableName: function (name) {
+		return name
+
+		// German umlauts
+		.replace(/ß/g, 'ss')
+		.replace(/ä/g, 'ae')
+		.replace(/ö/g, 'oe')
+		.replace(/ü/g, 'ue')
+
+		// remove abbreviations
+		.replace(/^[SU](\+[SU])?\W/, ' ')
+
+		// expand abbreviations
+		.toLowerCase()
+		.replace(/\(pl\)/g, ' polen ')
+		.replace(/\(berlin\)/g, ' berlin ')
+		.replace(/\(bln\)/g, ' berlin ')
+		.replace(/str\.?$/, 'strasse')
+		.replace(/str\.?\W/g, 'strasse ')
+		.replace(/bhf\.?$/, 'bahnhof')
+		.replace(/bhf\.?\W/g, 'bahnhof ')
+
+		// special chars
+		.replace(/[^\w\s]/g, ' ')
+		.replace(/_/g, ' ')
+
+		.replace(/\s{2,}/g, ' ')
+		.trim();
+	},
+
+
+
 	stringifyId: function (id) {
 		return id + '';
 	},
