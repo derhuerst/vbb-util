56632bbc064e047472cf0b36c1b547ac2805aa6c
Jannis R <mail@jannisr.de>
2015-09-10 03:47:36 +0200

    renamed `src/routes` to `src/lines`

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = {
 
 	locations:	require('./locations/index'),
-	routes:		require('./routes/index'),
+	lines:		require('./lines/index'),
 	apiErrors:	require('./api-errors'),
 	dateTime:	require('./date-time'),
 	duration:	require('./duration'),
