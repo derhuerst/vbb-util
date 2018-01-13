cd50d06bd25d734d16471357fcc0d4b027d57072
Jannis R <mail@jannisr.de>
2018-01-13 22:02:40 +0100

    remove api-errors.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const util = {
 
 	  locations: require('./locations/index')
 	, lines:     require('./lines/index')
-	, apiErrors: require('./api-errors')
 	, dateTime:  require('./date-time')
 	, duration:  require('./duration')
 	, products:  require('./products')
